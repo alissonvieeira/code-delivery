51211335eaf33d102b1b534f32e70d4a3b4aba81
Alisson Vieira <alisson.echo@gmail.com>
2017-10-12 20:58:19 -0300

    Criando base do sistema

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use CodeDelivery\Models\User;
+use CodeDelivery\Models\Client;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class, 10)->create()->each(function($user) {
+            $user->client()->save(factory(Client::class)->make());
+        });
+    }
+}
